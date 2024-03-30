dc9999f8d339e4de9dfff5919d9b9c8ba50cc488
Matt <matt@rcsipublishing.com>
2024-03-29 23:42:10 -0400

    Added public key to Actor

--- src/Type/Actor/Component/PublicKey.php
+++ src/Type/Actor/Component/PublicKey.php
@@ -11,6 +11,60 @@ class PublicKey extends APObject implements APObjectInterface
     protected ?string $owner;
     protected ?string $publicKeyPem;
 
+    /**
+     * @return string|null
+     */
+    public function getId(): ?string
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param string|null $id
+     * @return PublicKey
+     */
+    public function setId(?string $id): PublicKey
+    {
+        $this->id = $id;
+        return $this;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getOwner(): ?string
+    {
+        return $this->owner;
+    }
+
+    /**
+     * @param string|null $owner
+     * @return PublicKey
+     */
+    public function setOwner(?string $owner): PublicKey
+    {
+        $this->owner = $owner;
+        return $this;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getPublicKeyPem(): ?string
+    {
+        return $this->publicKeyPem;
+    }
+
+    /**
+     * @param string|null $publicKeyPem
+     * @return PublicKey
+     */
+    public function setPublicKeyPem(?string $publicKeyPem): PublicKey
+    {
+        $this->publicKeyPem = $publicKeyPem;
+        return $this;
+    }
+
 
 
 
