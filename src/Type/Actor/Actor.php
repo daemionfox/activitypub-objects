e96ef3a4643c98cb9300ed26a82bd1075ffad31f
Matt <matt@rcsipublishing.com>
2024-03-29 20:08:31 -0400

    Added public key to Actor

--- src/Type/Actor/Actor.php
+++ src/Type/Actor/Actor.php
@@ -10,4 +10,24 @@ abstract class Actor extends APObject implements APObjectInterface
 
     protected ?array $publicKey;
 
+    /**
+     * @return array|null
+     */
+    public function getPublicKey(): ?array
+    {
+        return $this->publicKey;
+    }
+
+    /**
+     * @param array|null $publicKey
+     * @return Actor
+     */
+    public function setPublicKey(?array $publicKey): Actor
+    {
+        $this->publicKey = $publicKey;
+        return $this;
+    }
+
+
+
 }
